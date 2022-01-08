89d738e88fb4da54f46ac5964abe18666ed9a161
tlh38 <tlh38@students.ptcollege.edu>
2022-01-07 22:13:59 -0500

    Starting Midterm Project

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>Test123</h1>
+
+</body>
+
+</html>
+
+<?php for ($i = 0; $i < 30; $i++)
+{
+    echo $i . "<br>";
+}
+?>
+
+<script>
+    let i = 12;
+    for (let i = 0; i < 12; i++) {
+        console.log(i);
+    }
+</script>
