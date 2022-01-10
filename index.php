1da0b2baacac10d5b21c9897d6d6e56a24560fc2
tlh38 <tlh38@students.ptcollege.edu>
2022-01-09 21:58:10 -0500

    Added config file & .gitignore to ignore said config file. Config file will track confidential info for php.

--- index.php
+++ index.php
@@ -9,21 +9,31 @@
 </head>
 
 <body>
-    <h1>Test123</h1>
+    <?php
+    include 'config.php';
+    $connectionInfo = array("UID" => "tlh38", "pwd" => $connPassword, "Database" => "tlh38PTC", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+    $serverName = "tcp:tlh38.database.windows.net,1433";
+    $conn = sqlsrv_connect($serverName, $connectionInfo);
+
+    if (!$conn)
+    {
+        echo '<script>alert("Connection failed to establish!")</script>';
+        die(print_r(sqlsrv_errors(), true));
+    }
+
+    $sql = "SELECT * FROM CUSTOMER";
+    $result = sqlsrv_query($conn, $sql);
+    if ($result == FALSE)
+        die(FormatErrors(sqlsrv_errors()));
+
+    echo ("<select>");
+    while ($row = sqlsrv_fetch_array($result, SQLSRV_FETCH_ASSOC))
+    {
+        echo ('<option value="' . $row['CUSTID'] . '"> ' . $row['FNAME'] . ' </option>');
+    }
+    echo ("</select>");
+    ?>
 
 </body>
 
 </html>
-
-<?php for ($i = 0; $i < 30; $i++)
-{
-    echo $i . "<br>";
-}
-?>
-
-<script>
-    let i = 12;
-    for (let i = 0; i < 12; i++) {
-        console.log(i);
-    }
-</script>
